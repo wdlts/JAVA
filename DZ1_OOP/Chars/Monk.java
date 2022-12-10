2d09c2313f0c021512bae1152b2ab81cbdde9bb9
sergeyt <sergej.tokarev@gmail.com>
2022-12-10 21:31:37 +0300

    DZ6

--- DZ1_OOP/Chars/Monk.java
+++ DZ1_OOP/Chars/Monk.java
@@ -14,9 +14,24 @@ public class Monk extends Heal {
     @Override
     public void step(ArrayList<BaseHero> side) {
         for (BaseHero baseHero : side) {
-            if (baseHero.health <= baseHero.minHP && baseHero.health !=0 && !baseHero.getClass().getSimpleName().equals("Peasant")) {
-                baseHero.health = baseHero.health - baseHero.damage[0];
+//            if (baseHero.health <= baseHero.minHP && baseHero.health !=0 && !baseHero.getClass().getSimpleName().equals("Peasant")) {
+//                baseHero.health = baseHero.health - baseHero.damage[0];
+//            }
+            if (baseHero.health < baseHero.maxHP*0.75){
+                baseHero.health = baseHero.health - this.getDamage()[0];{
+                    if (baseHero.health>baseHero.maxHP){
+                        baseHero.health=baseHero.maxHP;
+                    }
+                }
             }
+            if (baseHero.getStatus().equals(Statuses.DEAD) && baseHero.side.equals(this.side)) {
+                baseHero.status=Statuses.ALIVE;
+                baseHero.health = 1;
+            }
+//          if (baseHero.health > baseHero.getHealth()*0.75 && baseHero.side != this.side) {
+//              baseHero.health = baseHero.health - baseHero.damage[0];
+//          }
+
         }
 
     }
--- DZ1_OOP/Chars/Wizard.java
+++ DZ1_OOP/Chars/Wizard.java
@@ -11,18 +11,23 @@ public class Wizard extends Heal {
         super.side = side;
         super.position = new Vector2(x,y);
     }
+
     @Override
     public void step(ArrayList<BaseHero> side) {
         for (BaseHero baseHero : side) {
-            if (baseHero.health <= baseHero.minHP && baseHero.health !=0 && !baseHero.getClassName().equals("Peasant")) {
-                baseHero.health = baseHero.health - baseHero.damage[0];
-            }
-//            if (baseHero.health < baseHero.getHealth()*0.75 && baseHero.side != this.side){
-//                baseHero.health = baseHero.health + baseHero.damage[0];
+//            if (baseHero.health <= baseHero.minHP && baseHero.health !=0 && !baseHero.getClassName().equals("Peasant")) {
+//                baseHero.health = baseHero.health - baseHero.damage[0];
 //            }
-            if (baseHero.getStatus().equals(Statuses.DEAD) && baseHero.side == this.side) {
-                baseHero.health = 1;
+            if (baseHero.health < baseHero.maxHP*0.75){
+                baseHero.health = baseHero.health - this.getDamage()[0];{
+                    if (baseHero.health>baseHero.maxHP){
+                        baseHero.health=baseHero.maxHP;
+                    }
+                }
+            }
+            if (baseHero.getStatus().equals(Statuses.DEAD) && baseHero.side.equals(this.side)) {
                 baseHero.status=Statuses.ALIVE;
+                baseHero.health = 1;
             }
 //            if (baseHero.health > baseHero.getHealth()*0.75 && baseHero.side != this.side) {
 //                baseHero.health = baseHero.health - baseHero.damage[0];
